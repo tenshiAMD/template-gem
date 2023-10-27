a46d9e2b704a22f0ec57df00a2c7990c95076cf8
Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>
2023-10-27 21:34:24 +0800

    chore: initial commit

---
+++ lib/template/gem.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gem/version"
+
+module Template
+  module Gem
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/template/gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Template::Gem do
+  it "has a version number" do
+    expect(Template::Gem::VERSION).not_to be nil
+  end
+
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+end
